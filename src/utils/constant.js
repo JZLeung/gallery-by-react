6acb9622fc57e4f17180ef2023dc670f8c67db66
Leung-JZ <375082677@qq.com>
2017-11-28 17:24:06 +0800

    添加各个辅助函数的注释。

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,8 +1,14 @@
+/**
+ * 生成图片位置范围函数
+ * 传入参数依次为 舞台宽，舞台高，图片宽
+ */
 export default (stageWidth, stageHeight, imgWidth, imgHeight) => {
+    // 保存舞台一半的宽高，和图片的一半宽高
     let stageHalfWidth = stageWidth / 2,
         stageHalfHeight = stageHeight / 2,
         imgHalfWidth = imgWidth / 2,
         imgHalfHeight = imgHeight / 2
+    // 上方图片放置范围
     let top = {
         x: [
             stageHalfWidth - imgHalfWidth,
@@ -13,10 +19,12 @@ export default (stageWidth, stageHeight, imgWidth, imgHeight) => {
             stageHalfHeight - imgHeight
         ]
     },
+    // 居中图片的位置
     center = {
         left: stageHalfWidth - imgHalfWidth,
         top: stageHalfHeight - imgHalfHeight
     },
+    // 两边图片的放置范围
     aside = {
         leftX: [
             0 - imgHalfWidth,
--- src/utils/image.js
+++ src/utils/image.js
@@ -2,6 +2,7 @@ import Mock from 'mockjs'
 
 const Random = Mock.Random
 
+// 获取图片，并随机生成图片的标题和描述
 export default (imageArr) => {
     return imageArr.map((image, index) => {
         return {
